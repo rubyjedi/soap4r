2d748fc5b4762e57857773046053b4683dd8937c
Hiroshi Nakamura <nakahiro@gmail.com>
2001-05-20 05:59:03 +0000

    NQXML document customization.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@83 23c4dcf6-64ea-0310-84d7-d424c7495ec0

---
+++ lib/soap/nqxmlDocument.rb
@@ -0,0 +1,74 @@
+=begin
+SOAP4R - NQXML document customization
+Copyright (C) 2001 NAKAMURA Hiroshi.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PRATICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+Ave, Cambridge, MA 02139, USA.
+=end
+
+require 'nqxml/document'
+
+
+module NQXML
+  Attr = Struct.new( "Attr", :nodeName, :nodeValue )
+
+  class Node
+    def Node.initializeWithChildren( name, attrs, children = nil )
+      newObj = self.new( Tag.new( name, {} ), nil )
+
+      if attrs
+	attrs.each do | attr |
+	  newObj.entity.attrs[ attr.nodeName ] = attr.nodeValue
+	end
+      end
+
+      if children
+	if children.is_a?( Array )
+	  children.each do | child |
+	    newObj.addChildNode( child )
+	  end
+	elsif children.is_a?( NQXML::Node )
+	  newObj.addChildNode( children )
+	else
+	  newObj.addChild( children )
+	end
+      end
+
+      newObj
+    end
+
+    alias childNodes children
+
+    def attributes
+      entity.attrs.collect { | key, value |
+	Attr.new( key, value )
+      }
+    end
+
+    def nodeName
+      entity.name
+    end
+
+    def nodeValue
+      entity.text
+    end
+
+    def addChildNode( childNode )
+      @children << childNode
+    end
+  end
+
+  class NamedEntity
+    alias nodeName name
+  end
+end
