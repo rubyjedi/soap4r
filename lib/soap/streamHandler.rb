381efdfdaf77cb4b3ac0309304b1e1a87e5ffa34
Hiroshi Nakamura <nakahiro@gmail.com>
2001-05-20 06:26:58 +0000

    streamHander.rb was streamHander_wo_http-access.rb.  Renamed. Changed module namespace.  SOAPFoo -> SOAP::Foo Added wiredump interface.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@92 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- lib/soap/streamHandler.rb
+++ lib/soap/streamHandler.rb
@@ -16,14 +16,16 @@ this program; if not, write to the Free Software Foundation, Inc., 675 Mass
 Ave, Cambridge, MA 02139, USA.
 =end
 
+require 'soap/soap'
+require 'uri'
 require 'socket'
 require 'timeout'
 
-require 'soap/soap'
-require 'http-access'
-require 'uri'
 
-class SOAPStreamHandler
+module SOAP
+
+
+class StreamHandler
   public
 
   attr_reader :endPoint
@@ -34,8 +36,12 @@ class SOAPStreamHandler
 end
 
 
-class SOAPHTTPPostStreamHandler < SOAPStreamHandler
-  public
+class HTTPPostStreamHandler < StreamHandler
+  include SOAP
+
+public
+  
+  attr_accessor :dumpDev
   
   MediaType = 'text/xml'
 
@@ -45,8 +51,9 @@ class SOAPHTTPPostStreamHandler < SOAPStreamHandler
 
   def initialize( endPointUri, proxy = nil )
     super( endPointUri )
-    @server = URI.create( endPointUri )
+    @server = endPointUri
     @proxy = proxy
+    @dumpDev = nil	# Set an IO to get wiredump.
   end
 
   def send( soapString, soapAction = nil )
@@ -64,49 +71,108 @@ class SOAPHTTPPostStreamHandler < SOAPStreamHandler
   private
 
   def sendPOST( soapString, soapAction )
+    server = URI.create( @server )
+    dumpDev = if @dumpDev && @dumpDev.respond_to?( "<<" )
+	@dumpDev
+      else
+	nil
+      end
+
     retryNo = NofRetry
-    drv = nil
     begin
-      drv = HTTPAccess.new( @server.host, @server.port, @proxy )
+      if @proxy
+	proxy = URI.create( @proxy )
+	s = TCPSocket.new( proxy.host, proxy.port )
+      else
+	s = TCPSocket.new( server.host, server.port )
+      end
     rescue
       retryNo -= 1
       if retryNo > 0
         puts 'Retrying connection ...' if $DEBUG
         retry
       end
-      raise
+      raise HTTPStreamError.new( 'Connection failed.' )
+    end
+
+    if @proxy
+      absPath = @server if @proxy
+    else
+      absPath = server.path.dup
+      absPath << '?' << server.query if server.query
     end
     action = "\"#{ soapAction }\""
-    requestHeaders = { 'SOAPAction' => action, 'Content-Type' => MediaType }
 
-    puts soapString if $DEBUG
-
+    header = {}
     begin
       timeout( CallTimeout ) do
-	relative_uri = @server.path.dup
-	if @server.query
-	  relative_uri << '?' << @server.query
+          postData =<<EOS
+POST #{ absPath } HTTP/1.0
+Host: #{ server.host }
+Connection: close
+Content-Length: #{ soapString.size }
+Content-Type: #{ MediaType }
+SOAPAction: #{ action }
+
+EOS
+        postData.gsub!( "\n", CRLF )
+
+        postData << soapString
+        s.write postData
+
+	if dumpDev
+	  @dumpDev << "= Wire dump\n"
+	  @dumpDev << "\n"
+	  @dumpDev << "== Request from SOAP4R.\n"
+	  @dumpDev << "\n"
+	  @dumpDev << postData
+	  @dumpDev << "\n\n"
+
+	  @dumpDev << "== Response from server.\n"
+	  @dumpDev << "\n"
 	end
-	if @server.fragment
-	  relative_uri << '#' << @server.fragment
-	end
-	drv.request_post( relative_uri, soapString, requestHeaders )
-	rh = drv.get_header
-	puts rh if $DEBUG
-	responseHeaders = {}
-	rh.each do | header |
-	  /^([^:]+):\s*([^\r]*)\r?\n/ =~ header
-	  responseHeaders[ $1.downcase ] = $2
-	end
-        if ( drv.code == '405' )
+
+        raise HTTPStreamError.new( 'Unexpected EOF...' ) if s.eof
+
+        # Parse HTTP header
+        version = nil
+        status = nil
+        reason = nil
+        begin
+          line = s.gets.chop
+          dumpDev << line << "\n" if dumpDev
+          Regexp.new( '^HTTP/(1.\d)\s+(\d+)\s+(.*)$' ) =~ line
+          version = $1
+          status = $2
+          reason = $3
+
+          lastKey = nil
+          lastValue = nil
+          while !s.eof
+            line = s.gets.chop
+	    dumpDev << line << "\n" if dumpDev
+            if ( /^$/ =~ line )
+              header[ lastKey ] = lastValue if lastKey
+              break
+            elsif ( /^([^:]+)\s*:\s*(.*)$/ =~ line )
+              header[ lastKey ] = lastValue if lastKey
+              lastKey = $1.downcase
+              lastValue = $2
+            else
+              lastValue << "\n" << line
+            end
+          end
+        end while ( !s.eof and version == '1.1' and status == '100' )
+
+        if ( status == '405' )
           # 405: Method Not Allowed
-          raise PostUnavailableError.new( "#{ drv.code }: #{ drv.message }" )
-        elsif ( drv.code != '200' )
-          raise HTTPStreamError.new( "#{ drv.code }: #{ drv.message }" )
-        elsif ( !responseHeaders.has_key?( 'content-type' ))
+          raise PostUnavailableError.new( "#{ status }: #{ reason }" )
+        elsif ( status != '200' )
+          raise HTTPStreamError.new( "#{ status }: #{ reason }" )
+        elsif ( !header.has_key?( 'content-type' ))
           raise HTTPStreamError.new( 'Content-type not found.' )
-        elsif ( /^#{ MediaType }(?:;.*)?/ !~ responseHeaders[ 'content-type' ] )
-          raise HTTPStreamError.new( 'Illegal content-type: ' << responseHeaders[ 'content-type' ] )
+	elsif ( /^#{ MediaType }(?:;.*)?/ !~ header[ 'content-type' ] )
+#          raise HTTPStreamError.new( 'Illegal content-type: ' << header[ 'content-type' ] )
         end
       end
     rescue TimeoutError
@@ -116,15 +182,18 @@ class SOAPHTTPPostStreamHandler < SOAPStreamHandler
     receiveString = ''
     begin
       timeout( ReadTimeout ) do
-	drv.get_data( 8124 ) do | data |
-	  receiveString << data
+	while !s.eof
+	  line = s.gets
+	  receiveString << line
+	  dumpDev << line << "\n" if dumpDev
 	end
       end
     rescue TimeoutError
       raise HTTPStreamError.new( 'Read timeout' )
     end
 
-    puts receiveString if $DEBUG
+    dumpDev << "\n\n" if dumpDev
+
     receiveString
   end
 
@@ -140,4 +209,11 @@ class SOAPHTTPPostStreamHandler < SOAPStreamHandler
       raise HTTPStreamError.new( "#{ status }: #{ reason }" )
     end
   end
+
+  private
+
+  CRLF = "\r\n"
+end
+
+
 end
