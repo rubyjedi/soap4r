64fcc7bccb8f24adc1016c4beef9b97faa52ea11
Hiroshi Nakamura <nakahiro@gmail.com>
2001-05-29 13:14:23 +0000

    Changed default endpoint.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@120 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- test/interop/clientSOAP4R.rb
+++ test/interop/clientSOAP4R.rb
@@ -4,12 +4,10 @@ $serverName = 'SOAP4R'
 
 require 'clientBase'
 
-#$server = 'http://dora/~nakahiro/server.cgi'
+$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/'
 
-#$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/'
-
-require 'soap/XMLSchemaDatatypes1999'
-$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/1999/'
+#require 'soap/XMLSchemaDatatypes1999'
+#$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/1999/'
 $soapAction = 'urn:soapinterop'
 
 drv = SOAP::Driver.new( Log.new( STDERR ), 'InteropApp', InterfaceNS, $server, $proxy, $soapAction )
