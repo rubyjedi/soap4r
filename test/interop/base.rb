4bcc7bff4bf6cab786c12c05fc612aa76598ac05
Hiroshi Nakamura <nakahiro@gmail.com>
2001-07-04 03:57:30 +0000

    Added interfaces for R2. Added MappingRegistry.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@168 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- test/interop/base.rb
+++ test/interop/base.rb
@@ -1,9 +1,11 @@
-InterfaceNS = "http://soapinterop.org/"
+require 'soap/rpcUtils'
+
+InterfaceNS = 'http://soapinterop.org/'
+TypeNS = 'http://soapinterop.org/xsd'
 
 
 class SOAPStruct
   include SOAP::Marshallable
-  @@typeNamespace = 'http://soapinterop.org/xsd'
 
   attr_reader :varInt, :varFloat, :varString
 
@@ -14,7 +16,7 @@ class SOAPStruct
   end
 
   def ==( rhs )
-    r = if rhs.is_a?( SOAPStruct )
+    r = if rhs.is_a?( self.type )
 	( self.varInt == rhs.varInt &&
 	self.varFloat == rhs.varFloat &&
 	self.varString == rhs.varString )
@@ -28,3 +30,93 @@ class SOAPStruct
     "#{ varInt }:#{ varFloat }:#{ varString }"
   end
 end
+
+
+class SOAPStructStruct
+  include SOAP::Marshallable
+
+  attr_reader :varInt, :varFloat, :varString, :varStruct
+
+  def initialize( varInt, varFloat, varString, varStruct = nil )
+    @varInt = varInt
+    @varFloat = varFloat
+    @varString = varString
+    @varStruct = varStruct
+  end
+
+  def ==( rhs )
+    r = if rhs.is_a?( self.type )
+	( self.varInt == rhs.varInt &&
+	self.varFloat == rhs.varFloat &&
+	self.varString == rhs.varString &&
+	self.varStruct == rhs.varStruct )
+      else
+	false
+      end
+    r
+  end
+
+  def to_s
+    "#{ varInt }:#{ varFloat }:#{ varString }:#{ varStruct }"
+  end
+end
+
+
+class SOAPArrayStruct
+  include SOAP::Marshallable
+
+  attr_reader :varInt, :varFloat, :varString, :varArray
+
+  def initialize( varInt, varFloat, varString, varArray = nil )
+    @varInt = varInt
+    @varFloat = varFloat
+    @varString = varString
+    @varArray = varArray
+  end
+
+  def ==( rhs )
+    r = if rhs.is_a?( self.type )
+	( self.varInt == rhs.varInt &&
+	self.varFloat == rhs.varFloat &&
+	self.varString == rhs.varString &&
+	self.varArray == rhs.varArray )
+      else
+	false
+      end
+    r
+  end
+
+  def to_s
+    "#{ varInt }:#{ varFloat }:#{ varString }:#{ varArray }"
+  end
+end
+
+
+class StringArray < Array
+  @typeName = XSD::StringLiteral
+  @typeNamespace = XSD::Namespace
+end
+
+
+class IntArray < Array
+  @typeName = XSD::IntLiteral
+  @typeNamespace = XSD::Namespace
+end
+
+
+class FloatArray < Array
+  @typeName = XSD::FloatLiteral
+  @typeNamespace = XSD::Namespace
+end
+
+
+class SOAPStructArray < Array
+  @typeName = 'SOAPStruct'
+  @typeNamespace = TypeNS
+end
+
+
+MappingRegistry = SOAP::RPCUtils::MappingRegistry.new
+MappingRegistry.set( ::SOAPStruct, ::SOAP::SOAPStruct, SOAP::RPCUtils::MappingRegistry::TypedStructFactory, [ TypeNS, "SOAPStruct" ] )
+MappingRegistry.set( ::SOAPStructStruct, ::SOAP::SOAPStruct, SOAP::RPCUtils::MappingRegistry::TypedStructFactory, [ TypeNS, "SOAPStructStruct" ] )
+MappingRegistry.set( ::SOAPArrayStruct, ::SOAP::SOAPStruct, SOAP::RPCUtils::MappingRegistry::TypedStructFactory, [ TypeNS, "SOAPArrayStruct" ] )
