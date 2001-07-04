cf9f44201d830059066d55ca9701047ea1987e4a
Hiroshi Nakamura <nakahiro@gmail.com>
2001-07-04 04:00:30 +0000

    Changed to 2001 schema.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@172 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- test/interop/client4S4C1.3.rb
+++ test/interop/client4S4C1.3.rb
@@ -3,9 +3,9 @@
 $serverName = '4S4C'
 
 require 'clientBase'
-require 'soap/XMLSchemaDatatypes1999'
 
 $server = 'http://soap.4s4c.com/ilab/soap.asp'
+
 $soapAction = 'urn:soapinterop'
 
 drv = SOAP::Driver.new( Log.new( STDERR ), 'InteropApp', InterfaceNS, $server, $proxy, $soapAction )
--- test/interop/clienteSoapServer.rb
+++ test/interop/clienteSoapServer.rb
@@ -3,9 +3,9 @@
 $serverName = 'eSoapServer'
 
 require 'clientBase'
-require 'soap/XMLSchemaDatatypes1999'
 
 $server = 'http://www.connecttel.com/cgi-bin/esoapserver.cgi'
+
 $soapAction = 'urn:soapinterop'
 
 drv = SOAP::Driver.new( Log.new( STDERR ), 'InteropApp', InterfaceNS, $server, $proxy, $soapAction )
