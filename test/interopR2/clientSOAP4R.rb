9d3d4ecdc785cba85a3e1fe334d94fbb1828997c
Hiroshi Nakamura <nakahiro@gmail.com>
2001-07-10 12:41:31 +0000

    Changed.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@227 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- test/interopR2/clientSOAP4R.rb
+++ test/interopR2/clientSOAP4R.rb
@@ -6,12 +6,13 @@ require 'clientBase'
 
 #$server = 'http://dora/~nakahiro/server.cgi'
 
-$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/'
-#$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/1999/'
-#require 'soap/XMLSchemaDatatypes1999'
+#$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/'
+$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/1999/'
+require 'soap/XMLSchemaDatatypes1999'
 
 drv = SOAP::Driver.new( Log.new( STDERR ), 'InteropApp', InterfaceNS, $server, $proxy, $soapAction )
 
 methodDef( drv )
 
-doTest( drv )
+doTestBase( drv )
+doTestGroupB( drv )
