98101c647e4158c9265788fd0eed934ba08d4d0f
Hiroshi Nakamura <nakahiro@gmail.com>
2001-07-14 10:42:57 +0000

    Version: 1.3.5
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@262 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- lib/soap/soap.rb
+++ lib/soap/soap.rb
@@ -19,7 +19,7 @@ Ave, Cambridge, MA 02139, USA.
 module SOAP
   public
 
-  Version = '1.3.4'
+  Version = '1.3.5'
 
   EnvelopeNamespace = 'http://schemas.xmlsoap.org/soap/envelope/'
   EncodingNamespace = 'http://schemas.xmlsoap.org/soap/encoding/'
