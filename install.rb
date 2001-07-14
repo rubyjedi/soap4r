cefe1021839a867553f5913672b9f0ca4b6ea1d0
Hiroshi Nakamura <nakahiro@gmail.com>
2001-07-14 10:44:12 +0000

    Added urb installation.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@265 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- install.rb
+++ install.rb
@@ -15,14 +15,26 @@ RV = CONFIG["MAJOR"] + "." + CONFIG["MINOR"]
 DSTPATH = CONFIG["sitedir"] + "/" +  RV 
 
 begin
+  unless FileTest.directory?( "lib/soap" )
+    raise RuntimeError.new( "'lib/soap' not found." )
+  end
+
+  unless FileTest.directory?( "redist" )
+    raise RuntimeError.new( "'redist' not found." )
+  end
+
   File.mkpath DSTPATH + "/soap", true 
-  
   Dir["lib/soap/*.rb"].each do |name|
     File.install name, "#{DSTPATH}/soap/#{File.basename name}", 0644, true
   end
 
   Dir["redist/*.rb"].each do |name|
     File.install name, "#{DSTPATH}/#{File.basename name}", 0644, true
+  end
+
+  File.mkpath DSTPATH + "/urb", true 
+  Dir["redist/urb/*.rb"].each do |name|
+    File.install name, "#{DSTPATH}/urb/#{File.basename name}", 0644, true
   end
 
 rescue 
