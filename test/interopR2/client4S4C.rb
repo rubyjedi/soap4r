ce62ef669362de7cc51ec2ddb7258b848e24f85c
nahi <nahi@ruby-lang.org>
2001-07-26 03:38:19 +0000

    Added some tests.

--- test/interopR2/client4S4C.rb
+++ test/interopR2/client4S4C.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
 $serverName = '4S4C'
+$server = 'http://soap.4s4c.com/ilab/soap.asp'
 
 require 'clientBase'
-
-$server = 'http://soap.4s4c.com/ilab/soap.asp'
 
 drv = SOAP::Driver.new( Log.new( STDERR ), 'InteropApp', InterfaceNS, $server, $proxy, $soapAction )
 
 methodDef( drv )
 
 doTest( drv )
+submitTestResult
--- test/interopR2/clientSOAP4R.rb
+++ test/interopR2/clientSOAP4R.rb
@@ -2,17 +2,19 @@
 
 $serverName = 'SOAP4R'
 
+$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/'
+#$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/1999/'
+#require 'soap/XMLSchemaDatatypes1999'
+
 require 'clientBase'
 
-#$server = 'http://dora/~nakahiro/server.cgi'
+log = Log.new( STDERR )
+log.sevThreshold = Log::SEV_INFO	# Log::SEV_WARN, Log::SEV_DEBUG
 
-#$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/'
-$server = 'http://www.jin.gr.jp/~nahi/Ruby/SOAP4R/SOAPBuildersInterop/1999/'
-require 'soap/XMLSchemaDatatypes1999'
-
-drv = SOAP::Driver.new( Log.new( STDERR ), 'InteropApp', InterfaceNS, $server, $proxy, $soapAction )
+drv = SOAP::Driver.new( log, 'InteropApp', InterfaceNS, $server, $proxy, $soapAction )
 
 methodDef( drv )
 
 doTestBase( drv )
 doTestGroupB( drv )
+submitTestResult
