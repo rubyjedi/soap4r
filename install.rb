939097937c6c79d6b7fe8c57634097551bc1b448
Hiroshi Nakamura <nakahiro@gmail.com>
2001-07-26 04:09:03 +0000

    Added http-access2.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@286 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- install.rb
+++ install.rb
@@ -23,8 +23,16 @@ begin
     raise RuntimeError.new( "'redist' not found." )
   end
 
+  unless FileTest.directory?( "redist/soap" )
+    raise RuntimeError.new( "'redist/soap' not found." )
+  end
+
   File.mkpath DSTPATH + "/soap", true 
   Dir["lib/soap/*.rb"].each do |name|
+    File.install name, "#{DSTPATH}/soap/#{File.basename name}", 0644, true
+  end
+
+  Dir["redist/soap/*.rb"].each do |name|
     File.install name, "#{DSTPATH}/soap/#{File.basename name}", 0644, true
   end
 
@@ -32,9 +40,16 @@ begin
     File.install name, "#{DSTPATH}/#{File.basename name}", 0644, true
   end
 
+  # Installing URb
   File.mkpath DSTPATH + "/urb", true 
   Dir["redist/urb/*.rb"].each do |name|
     File.install name, "#{DSTPATH}/urb/#{File.basename name}", 0644, true
+  end
+
+  # Installing http-access2
+  File.mkpath DSTPATH + "/http-access2", true 
+  Dir["redist/http-access2/*.rb"].each do |name|
+    File.install name, "#{DSTPATH}/http-access2/#{File.basename name}", 0644, true
   end
 
 rescue 
