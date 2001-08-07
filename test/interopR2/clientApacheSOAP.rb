17151aba1ebe992a51232cf6c5b405af3ea5f370
Hiroshi Nakamura <nakahiro@gmail.com>
2001-08-07 01:18:00 +0000

    echoMap
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@304 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- test/interopR2/clientApacheSOAP.rb
+++ test/interopR2/clientApacheSOAP.rb
@@ -12,6 +12,8 @@ methodDefBase( drvBase )
 #drvGroupB = SOAP::Driver.new( Log.new( STDERR ), 'InteropApp', InterfaceNS, $serverGroupB, $proxy, $soapAction )
 #methodDefGroupB( drvGroupB )
 
+$test_echoMap = true
+
 doTestBase( drvBase )
 #doTestGroupB( drvGroupB )
 submitTestResult
