da26a27081a7cdb3043b80516c87b840f613d3d5
Hiroshi Nakamura <nakahiro@gmail.com>
2002-02-10 17:39:53 +0000

    Separate iRAA.rb into RAA.rb and iRAA.rb.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@417 23c4dcf6-64ea-0310-84d7-d424c7495ec0

---
+++ sample/RAA/RAA.rb
@@ -0,0 +1,61 @@
+require 'soap/driver'
+require 'devel/logger'
+require 'iRAA'
+
+module RAA; extend SOAP
+
+
+class Driver
+  attr_reader :endpointUrl
+  attr_reader :proxy
+
+  attr_reader :log
+
+  AppName = 'RAAClient'
+
+  def initialize( endpointUrl, proxy = nil )
+    @endpointUrl = endpointUrl
+    @proxy = proxy
+    @logDev = STDERR
+  end
+
+  def setWireDumpDev( dev )
+    @drv = createDriver unless @drv
+    @drv.setWireDumpDev( dev )
+  end
+
+  def setLogDev( logDev )
+    @logDev = logDev
+    @log = nil
+  end
+
+  def setLogLevel( level )
+    createLog unless @log
+    @log.level = level
+  end
+
+private
+
+  def createLog
+    @log = Devel::Logger.new( @logDev )
+  end
+
+  def createDriver
+    createLog unless @log
+    drv = SOAP::Driver.new( @log, AppName, RAA::InterfaceNS, @endpointUrl,
+      @proxy )
+    drv.mappingRegistry = RAA::MappingRegistry
+    RAA::Methods.each do | methodName, *params |
+      drv.addMethod( methodName, params )
+    end
+    drv
+  end
+
+  def method_missing( msg_id, *a, &b )
+    @drv = createDriver unless @drv
+    @drv.__send__( msg_id, *a, &b )
+  end
+end
+
+
+end
