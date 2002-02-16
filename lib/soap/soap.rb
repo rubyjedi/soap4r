bd42c310ca7b3dacb2a34de050348d96c72ee2d5
nahi <nahi@ruby-lang.org>
2002-02-16 15:55:37 +0000

    Modified indentation.

--- lib/soap/soap.rb
+++ lib/soap/soap.rb
@@ -1,79 +1,78 @@
-=begin
-SOAP4R - Base definitions.
-Copyright (C) 2000, 2001 NAKAMURA Hiroshi.
+# SOAP4R - Base definitions.
+# Copyright (C) 2000, 2001 NAKAMURA Hiroshi.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PRATICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+# Ave, Cambridge, MA 02139, USA.
 
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PRATICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 675 Mass
-Ave, Cambridge, MA 02139, USA.
-=end
 
 module SOAP
-  public
 
-  Version = '1.4.2'
 
-  EnvelopeNamespace = 'http://schemas.xmlsoap.org/soap/envelope/'
-  EncodingNamespace = 'http://schemas.xmlsoap.org/soap/encoding/'
-  LiteralNamespace = 'http://xml.apache.org/xml-soap/literalxml'
+Version = '1.4.2'
 
-  NextActor = 'http://schemas.xmlsoap.org/soap/actor/next'
+EnvelopeNamespace = 'http://schemas.xmlsoap.org/soap/envelope/'
+EncodingNamespace = 'http://schemas.xmlsoap.org/soap/encoding/'
+LiteralNamespace = 'http://xml.apache.org/xml-soap/literalxml'
 
-  AttrMustUnderstand = 'mustUnderstand'
-  AttrEncodingStyle = 'encodingStyle'
-  AttrActor = 'actor'
+NextActor = 'http://schemas.xmlsoap.org/soap/actor/next'
 
-  AttrRoot = 'root'
-  AttrArrayType = 'arrayType'
-  AttrOffset = 'offset'
-  AttrPosition = 'position'
+AttrMustUnderstand = 'mustUnderstand'
+AttrEncodingStyle = 'encodingStyle'
+AttrActor = 'actor'
 
-  Base64Literal = 'base64'
+AttrRoot = 'root'
+AttrArrayType = 'arrayType'
+AttrOffset = 'offset'
+AttrPosition = 'position'
 
-  SOAPNamespaceTag = 'env'
-  XSDNamespaceTag = 'xsd'
-  XSINamespaceTag = 'xsi'
+Base64Literal = 'base64'
 
-  class Error < StandardError; end
+SOAPNamespaceTag = 'env'
+XSDNamespaceTag = 'xsd'
+XSINamespaceTag = 'xsi'
 
-  class StreamError < Error; end
-  class HTTPStreamError < StreamError; end
-  class PostUnavailableError < HTTPStreamError; end
-  class MPostUnavailableError < HTTPStreamError; end
+class Error < StandardError; end
 
-  class ArrayIndexOutOfBoundsError < Error; end
-  class ArrayStoreError < Error; end
+class StreamError < Error; end
+class HTTPStreamError < StreamError; end
+class PostUnavailableError < HTTPStreamError; end
+class MPostUnavailableError < HTTPStreamError; end
 
-  class FaultError < Error
-    public
+class ArrayIndexOutOfBoundsError < Error; end
+class ArrayStoreError < Error; end
 
-    attr_reader :faultCode
-    attr_reader :faultString
-    attr_reader :faultActor
-    attr_accessor :detail
+class FaultError < Error
+  attr_reader :faultCode
+  attr_reader :faultString
+  attr_reader :faultActor
+  attr_accessor :detail
 
-    def initialize( fault )
-      @faultCode = fault.faultcode
-      @faultString = fault.faultstring
-      @faultActor = fault.faultactor
-      @detail = fault.detail
-      super( self.to_s )
-    end
-
-    def to_s
-      str = nil
-      if @faultString && @faultString.respond_to?( 'data' )
-	str = @faultString.data
-      end
-      str || '(No faultString)'
-    end
+  def initialize( fault )
+    @faultCode = fault.faultcode
+    @faultString = fault.faultstring
+    @faultActor = fault.faultactor
+    @detail = fault.detail
+    super( self.to_s )
   end
+
+  def to_s
+    str = nil
+    if @faultString && @faultString.respond_to?( 'data' )
+      str = @faultString.data
+    end
+    str || '(No faultString)'
+  end
+end
+
+
 end
