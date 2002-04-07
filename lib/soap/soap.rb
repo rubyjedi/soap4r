1e41446cf1ce0f3ecac34ec1e6428b971fa2b3dd
nahi <nahi@ruby-lang.org>
2002-04-07 16:09:53 +0000

    Version = '1.4.3'

--- lib/soap/soap.rb
+++ lib/soap/soap.rb
@@ -18,7 +18,7 @@
 module SOAP
 
 
-Version = '1.4.2'
+Version = '1.4.3'
 
 EnvelopeNamespace = 'http://schemas.xmlsoap.org/soap/envelope/'
 EncodingNamespace = 'http://schemas.xmlsoap.org/soap/encoding/'
