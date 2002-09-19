85929985d5b9ee41dca8b7e6a4b32344aaf939b4
Hiroshi Nakamura <nakahiro@gmail.com>
2002-09-19 15:10:15 +0000

    Use getoptlong. Added several creators.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@578 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- bin/wsdl2ruby.rb
+++ bin/wsdl2ruby.rb
@@ -2,58 +2,232 @@
 
 require 'getoptlong'
 require 'wsdl/xmlparser'
+require 'wsdl/name'
 require 'wsdl/soap/classDefCreator'
-require 'wsdl/soap/stubCreator'
+require 'wsdl/soap/servantSkeltonCreator'
 require 'wsdl/soap/driverCreator'
+require 'wsdl/soap/clientSkeltonCreator'
+require 'wsdl/soap/standaloneServerStubCreator'
+require 'wsdl/soap/cgiStubCreator'
+require 'wsdl/soap/webrickStubCreator'
 
-def usageExit
-  puts "Usage: #{ $0 } --type server|client wsdlfilename"
-  exit 1
-end
-opt = GetoptLong.new (
-  ['--type','-t', GetoptLong::REQUIRED_ARGUMENT]
-)
+require 'devel/logger'
 
-server = client = false
-begin
-  opt.each do | name, arg |
-    case name
-    when "--type"
-      case arg
-      when "server"
-	server = true
-      when "client"
-	client = true
-      else
-	raise ArgumentError.new( "Unknown type #{ arg }" )
+class WSDL2RubyApp < Devel::Application
+private
+
+  OptSet = [
+    ['--wsdl','-w', GetoptLong::REQUIRED_ARGUMENT],
+    ['--type','-t', GetoptLong::REQUIRED_ARGUMENT],
+    ['--classDef','-f', GetoptLong::NO_ARGUMENT],
+    ['--clientSkelton','-c', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--servantSkelton','-s', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--cgiStub','-g', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--webrickStub','-b', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--standaloneServerStub','-a', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--driver','-d', GetoptLong::OPTIONAL_ARGUMENT]
+  ]
+
+  def initialize
+    super( 'app' )
+    @wsdlFile = nil
+    @opt = {}
+    @wsdl = nil
+    @name = nil
+  end
+
+  def run
+    @wsdlFile, @opt = parseOpt( GetoptLong.new( *OptSet ))
+    usageExit unless @wsdlFile
+    @wsdl = WSDL::WSDLXMLParser.new.parse( File.open( @wsdlFile ))
+    @name = @wsdl.name.name || 'default'
+    createFile
+    0
+  end
+
+  def createFile
+    createClassDef if @opt.has_key?( 'classDef' )
+    createServantSkelton( @opt[ 'servantSkelton' ] ) if @opt.has_key?( 'servantSkelton' )
+    createCgiStub( @opt[ 'cgiStub' ] ) if @opt.has_key?( 'cgiStub' )
+    createWebrickStub( @opt[ 'webrickStub' ] ) if @opt.has_key?( 'webrickStub' )
+    createStandaloneServerStub( @opt[ 'standaloneServerStub' ] ) if @opt.has_key?( 'standaloneServerStub' )
+    createDriver( @opt[ 'driver' ] ) if @opt.has_key?( 'driver' )
+    createClientSkelton( @opt[ 'clientSkelton' ] ) if @opt.has_key?( 'clientSkelton' )
+  end
+
+  def usageExit
+    puts <<__EOU__
+Usage: #{ $0 } --wsdl wsdlFilename [options]
+
+Example:
+  For server side:
+    #{ $0 } --wsdl myApp.wsdl --type server
+  For client side:
+    #{ $0 } --wsdl myApp.wsdl --type client
+
+Options:
+  --wsdl wsdlFilename
+  --type server|client
+    --type server implies;
+  	--classDef
+   	--servantSkelton
+    	--standaloneServerStub
+    --type client implies;
+     	--classDef
+      	--clientSkelton
+       	--driver
+  --classDef
+  --clientSkelton [serviceName]
+  --servantSkelton [portTypeName]
+  --cgiStub [serviceName]
+  --webrickStub [serviceName]
+  --standaloneServerStub [serviceName]
+  --driver [portTypeName]
+
+Terminology:
+  Client <-> Driver <-(SOAP)-> Stub <-> Servant
+
+  Driver and Stub: Automatically generated
+  Client and Servant: Skelton generated (you should change)
+__EOU__
+    exit 1
+  end
+
+  def parseOpt( getOpt )
+    opt = {}
+    wsdlFile = nil
+    begin
+      getOpt.each do | name, arg |
+       	case name
+	when "--wsdl"
+	  wsdlFile = arg
+	when "--type"
+  	  case arg
+  	  when "server"
+  	    opt[ 'classDef' ] = nil
+  	    opt[ 'servantSkelton' ] = nil
+  	    opt[ 'standaloneServerStub' ] = nil
+  	  when "client"
+  	    opt[ 'classDef' ] = nil
+  	    opt[ 'driver' ] = nil
+  	    opt[ 'clientSkelton' ] = nil
+  	  else
+  	    raise ArgumentError.new( "Unknown type #{ arg }" )
+  	  end
+   	when "--classDef", "--clientSkelton", "--servantSkelton", "--cgiStub",
+    	    "--webrickStub", "--standaloneServerStub", "--driver"
+  	  opt[ name.sub( /^--/, '' ) ] = arg.empty? ? nil : arg
+   	else
+  	  raise ArgumentError.new( "Unknown type #{ arg }" )
+   	end
       end
+    rescue
+      usageExit
+    end
+    return wsdlFile, opt
+  end
+
+  def createClassDef
+    log( SEV_INFO ) { "Creating class definition." }
+    @classDefFilename = @name + '.rb'
+    checkFile( @classDefFilename ) or return
+    File.open( @classDefFilename, "w" ) do | f |
+      f << WSDL::SOAP::ClassDefCreator.new( @wsdl ).dump
     end
   end
-rescue
-  usageExit
-end
-usageExit if !server and !client
 
-wsdlFile = ARGV.shift
-usageExit unless wsdlFile
+  def createClientSkelton( serviceName )
+    log( SEV_INFO ) { "Creating client skelton." }
+    serviceName ||= @wsdl.services[ 0 ].name.name
+    @clientSkeltonFilename = serviceName + 'Client.rb'
+    checkFile( @clientSkeltonFilename ) or return
+    File.open( @clientSkeltonFilename, "w" ) do | f |
+      f << shbang << "\n"
+      f << "require '#{ @driverFilename }'\n\n" if @driverFilename
+      f << WSDL::SOAP::ClientSkeltonCreator.new( @wsdl ).dump(
+	createName( serviceName ))
+    end
+  end
 
-wsdl = WSDL::WSDLXMLParser.new.parse( File.open( wsdlFile ))
+  def createServantSkelton( portTypeName )
+    log( SEV_INFO ) { "Creating servant skelton." }
+    @servantSkeltonFilename = ( portTypeName || @name + 'Servant' ) + '.rb'
+    checkFile( @servantSkeltonFilename ) or return
+    File.open( @servantSkeltonFilename, "w" ) do | f |
+      f << "require '#{ @classDefFilename }'\n\n" if @classDefFilename
+      f << WSDL::SOAP::ServantSkeltonCreator.new( @wsdl ).dump(
+	createName( portTypeName ))
+    end
+  end
 
-# Class definition
-File.open( "classDef.rb", "w" ) do | f |
-  f << WSDL::SOAP::ClassDefCreator.new( wsdl ).dump
-end
+  def createCgiStub( serviceName )
+    log( SEV_INFO ) { "Creating CGI stub." }
+    serviceName ||= @wsdl.services[ 0 ].name.name
+    @cgiStubFilename = serviceName + '.cgi'
+    checkFile( @cgiStubFilename ) or return
+    File.open( @cgiStubFilename, "w" ) do | f |
+      f << shbang << "\n"
+      f << "require '#{ @servantSkeltonFilename }'\n\n" if @servantSkeltonFilename
+      f << WSDL::SOAP::CGIStubCreator.new( @wsdl ).dump(
+	createName( serviceName ))
+    end
+  end
 
-if server
-  File.open( "servant.rb", "w" ) do | f |
-    f << "require 'classDef'\n\n"
-    f << WSDL::SOAP::StubCreator.new( wsdl ).dump
+  def createWebrickStub( serviceName )
+    log( SEV_INFO ) { "Creating WEBrick SOAPlet stub." }
+    serviceName ||= @wsdl.services[ 0 ].name.name
+    @webrickStubFilename = 'httpd.rb'
+    checkFile( @webrickStubFilename ) or return
+    File.open( @webrickStubFilename, "w" ) do | f |
+      f << shbang << "\n"
+      f << "require '#{ @servantSkeltonFilename }'\n\n" if @servantSkeltonFilename
+      f << WSDL::SOAP::WEBrickStubCreator.new( @wsdl ).dump(
+	createName( serviceName ))
+    end
+  end
+
+  def createStandaloneServerStub( serviceName )
+    log( SEV_INFO ) { "Creating standalone stub." }
+    serviceName ||= @wsdl.services[ 0 ].name.name
+    @standaloneServerStubFilename = serviceName + '.rb'
+    checkFile( @standaloneServerStubFilename ) or return
+    File.open( @standaloneServerStubFilename, "w" ) do | f |
+      f << shbang << "\n"
+      f << "require '#{ @servantSkeltonFilename }'\n\n" if @servantSkeltonFilename
+      f << WSDL::SOAP::StandaloneServerStubCreator.new( @wsdl ).dump(
+	createName( serviceName ))
+    end
+  end
+
+  def createDriver( portTypeName )
+    log( SEV_INFO ) { "Creating driver." }
+    @driverFilename = ( portTypeName || @name ) + 'Driver.rb'
+    checkFile( @driverFilename ) or return
+    File.open( @driverFilename, "w" ) do | f |
+      f << "require '#{ @classDefFilename }'\n\n" if @classDefFilename
+      f << WSDL::SOAP::DriverCreator.new( @wsdl ).dump(
+	createName( portTypeName ))
+    end
+  end
+
+  def checkFile( filename )
+    if FileTest.exist?( filename )
+      log( SEV_WARN ) {
+	"File '#{ filename }' exists.  #{ $0 } did not override it." }
+      false
+    else
+      log( SEV_INFO ) { "Creates file '#{ filename }'." }
+      true
+    end
+  end
+
+  def shbang
+    "#!/usr/bin/env ruby"
+  end
+
+  def createName( name )
+    name ? WSDL::Name.new( @wsdl.targetNamespace, name ) : nil
   end
 end
 
-if client
-  File.open( "driver.rb", "w" ) do | f |
-    f << "require 'classDef'\n\n"
-    f << WSDL::SOAP::DriverCreator.new( wsdl ).dump
-  end
-end
+WSDL2RubyApp.new.start
