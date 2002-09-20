2b7d148ec3dda1c71640cde9941d5fd785cde2d6
Hiroshi Nakamura <nakahiro@gmail.com>
2002-09-20 17:13:03 +0000

    Added XMLScan support.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@586 23c4dcf6-64ea-0310-84d7-d424c7495ec0

---
+++ lib/soap/xmlscanner.rb
@@ -0,0 +1,90 @@
+=begin
+SOAP4R - SOAP XMLScan parser library.
+Copyright (C) 2002 NAKAMURA Hiroshi.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PRATICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+Ave, Cambridge, MA 02139, USA.
+=end
+
+require 'soap/parser'
+require 'xmlscan/scanner'
+
+
+module SOAP
+
+
+class SOAPXMLScanner < SOAPParser
+  def initialize( *vars )
+    super( *vars )
+  end
+
+  def self.adjustKCode
+    true
+  end
+
+  def prologue
+    @charsetStrBackup = $KCODE.to_s.dup
+  end
+
+  def doParse( stringOrReadable )
+    Scanner.new( self ).parse( stringOrReadable )
+  end
+
+  class Scanner < XMLScan::XMLScanner
+    def initialize( dest )
+      super()
+      @dest = dest
+    end
+
+    def on_stag( name, attr )
+      @dest.startElement( name, attr )
+    end
+  
+    def on_etag( name )
+      @dest.endElement( name )
+    end
+
+    def on_chardata( str )
+      @dest.characters( str )
+    end
+
+    ENTITY_REF_MAP = { 'lt' => '<', 'gt' => '>', 'amp' => '&', 'quot' => '"', 'apos' => '\'' }
+    def on_entityref( ref )
+      @dest.characters( ENTITY_REF_MAP[ ref ] )
+    end
+
+    def on_charref( code )
+      @dest.characters( [ Integer( code ) ].pack( "U*" ))
+    end
+
+    def on_xmldecl( decls )
+      encTag = decls.find { | decl | decl[ 0 ] == 'encoding' }
+      if encTag
+	charsetStr = Charset.getCharsetStr( encTag[ 1 ] )
+     	@charsetStrBackup = $KCODE.to_s.dup
+  	$KCODE = charsetStr
+	Charset.setXMLInstanceEncoding( charsetStr )
+      end
+    end
+  end
+
+  def epilogue
+    $KCODE = @charsetStrBackup
+    Charset.setXMLInstanceEncoding( $KCODE )
+  end
+
+  setFactory( self )
+end
+
+
+end
