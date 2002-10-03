8244533cd34a5218e855ae33cf20f546557fed9a
Hiroshi Nakamura <nakahiro@gmail.com>
2002-10-03 16:21:31 +0000

    Separate soap related definitions from wsdl/definition.rb
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@626 23c4dcf6-64ea-0310-84d7-d424c7495ec0

---
+++ lib/wsdl/soap/definitions.rb
@@ -0,0 +1,59 @@
+=begin
+WSDL4R - WSDL additional definitions for SOAP.
+Copyright (C) 2002 NAKAMURA Hiroshi.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PRATICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+Ave, Cambridge, MA 02139, USA.
+=end
+
+
+require 'wsdl/info'
+
+
+module WSDL
+
+
+class Definitions < Info
+  def getComplexTypesWithMessages
+    types = complexTypes
+    messages.each do | message |
+      type = createComplexType( message.name )
+      message.parts.each do | part |
+	type.addElement( part.name, part.type )
+      end
+      types << type
+    end
+    types << faultComplexType
+    types
+  end
+
+private
+
+  def faultComplexType
+    type = createComplexType( ::SOAP::EleFaultName )
+    type.addElement( ::SOAP::EleFaultCodeName.name, XSD::XSDString::Type )
+    type.addElement( ::SOAP::EleFaultStringName.name, XSD::XSDString::Type )
+    type.addElement( ::SOAP::EleFaultActorName.name, XSD::XSDString::Type )
+    type.addElement( ::SOAP::EleFaultDetailName.name, XSD::XSDAnyType::Type )
+    type
+  end
+
+  def createComplexType( typeQName )
+    type = XMLSchema::ComplexType.new
+    type.setAnonymousTypeName( typeQName )
+    type
+  end
+end
+
+
+end
