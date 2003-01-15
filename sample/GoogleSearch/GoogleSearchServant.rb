3a0738fc68bdd0b3978a1c5e732a49ad2574e706
Hiroshi Nakamura <nakahiro@gmail.com>
2003-01-15 15:25:12 +0000

    Update generated files.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@724 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- sample/GoogleSearch/GoogleSearchServant.rb
+++ sample/GoogleSearch/GoogleSearchServant.rb
@@ -5,11 +5,11 @@ class GoogleSearchPort
   #   doGetCachedPage( key, url )
   #
   # ARGS
-  #   key		{http://www.w3.org/2001/XMLSchema}string
-  #   url		{http://www.w3.org/2001/XMLSchema}string
+  #   key		String - {http://www.w3.org/2001/XMLSchema}string
+  #   url		String - {http://www.w3.org/2001/XMLSchema}string
   #
   # RETURNS
-  #   return		{http://www.w3.org/2001/XMLSchema}base64Binary
+  #   return		 - {http://www.w3.org/2001/XMLSchema}base64Binary
   #
   # RAISES
   #    N/A
@@ -22,11 +22,11 @@ class GoogleSearchPort
   #   doSpellingSuggestion( key, phrase )
   #
   # ARGS
-  #   key		{http://www.w3.org/2001/XMLSchema}string
-  #   phrase		{http://www.w3.org/2001/XMLSchema}string
+  #   key		String - {http://www.w3.org/2001/XMLSchema}string
+  #   phrase		String - {http://www.w3.org/2001/XMLSchema}string
   #
   # RETURNS
-  #   return		{http://www.w3.org/2001/XMLSchema}string
+  #   return		String - {http://www.w3.org/2001/XMLSchema}string
   #
   # RAISES
   #    N/A
@@ -39,19 +39,19 @@ class GoogleSearchPort
   #   doGoogleSearch( key, q, start, maxResults, filter, restrict, safeSearch, lr, ie, oe )
   #
   # ARGS
-  #   key		{http://www.w3.org/2001/XMLSchema}string
-  #   q		{http://www.w3.org/2001/XMLSchema}string
-  #   start		{http://www.w3.org/2001/XMLSchema}int
-  #   maxResults		{http://www.w3.org/2001/XMLSchema}int
-  #   filter		{http://www.w3.org/2001/XMLSchema}boolean
-  #   restrict		{http://www.w3.org/2001/XMLSchema}string
-  #   safeSearch		{http://www.w3.org/2001/XMLSchema}boolean
-  #   lr		{http://www.w3.org/2001/XMLSchema}string
-  #   ie		{http://www.w3.org/2001/XMLSchema}string
-  #   oe		{http://www.w3.org/2001/XMLSchema}string
+  #   key		String - {http://www.w3.org/2001/XMLSchema}string
+  #   q		String - {http://www.w3.org/2001/XMLSchema}string
+  #   start		Integer - {http://www.w3.org/2001/XMLSchema}int
+  #   maxResults		Integer - {http://www.w3.org/2001/XMLSchema}int
+  #   filter		TrueClass - {http://www.w3.org/2001/XMLSchema}boolean
+  #   restrict		String - {http://www.w3.org/2001/XMLSchema}string
+  #   safeSearch		TrueClass - {http://www.w3.org/2001/XMLSchema}boolean
+  #   lr		String - {http://www.w3.org/2001/XMLSchema}string
+  #   ie		String - {http://www.w3.org/2001/XMLSchema}string
+  #   oe		String - {http://www.w3.org/2001/XMLSchema}string
   #
   # RETURNS
-  #   return		{urn:GoogleSearch}GoogleSearchResult
+  #   return		GoogleSearchResult - {urn:GoogleSearch}GoogleSearchResult
   #
   # RAISES
   #    N/A
