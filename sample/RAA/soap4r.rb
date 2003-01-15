8b8551471622232533c4285cdcb80f457262f828
nahi <nahi@ruby-lang.org>
2003-01-15 13:08:57 +0000

    pp is not used.

--- sample/RAA/soap4r.rb
+++ sample/RAA/soap4r.rb
@@ -1,30 +1,22 @@
 #!/usr/bin/env ruby
 
 require 'RAA'
-require 'pp'
 require 'soap/marshal'
 
-server = 'http://www.ruby-lang.org/~nahi/soap/raa/'
+server = ARGV.shift || 'http://www.ruby-lang.org/~nahi/soap/raa/'
 proxy = ENV[ 'HTTP_PROXY' ] || ENV[ 'http_proxy' ]
 
 raa = RAA::Driver.new( server, proxy )
 raa.setLogDev( nil )
-
-# targetDate = DateTime.civil( 2002, 5, 7 )
-# from = targetDate - targetDate.wday
-# to = from + 7
-# ( raa.getModifiedInfoSince( from ) - raa.getModifiedInfoSince( to )).each do | info |
-#   p info
-# end
 
 p raa.getAllListings().sort
 
 p raa.getProductTree()
 
 p raa.getInfoFromCategory( RAA::Category.new( "Library", "XML" ))
-
 t = Time.at( Time.now.to_i - 24 * 3600 )
 p raa.getModifiedInfoSince( t )
+
 p raa.getModifiedInfoSince( DateTime.new( t.year, t.mon, t.mday, t.hour, t.min, t.sec ))
 
 o = raa.getInfoFromName( "SOAP4R" )
