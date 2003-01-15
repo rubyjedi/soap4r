24705ec6fcce03ccfab447c5ee85b904e5084cf2
nahi <nahi@ruby-lang.org>
2003-01-15 13:07:24 +0000

    RNN SOAP interface sample.

---
+++ sample/RNN/client.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+
+# RNNのSOAPサービスを利用するサンプルです。詳細については、
+# http://cvs.sourceforge.jp/cgi-bin/viewcvs.cgi/*checkout*/rnn/rnn/doc/articles/xmlrpc.txt
+# および
+# http://rwiki.jin.gr.jp/cgi-bin/rw-cgi.rb?cmd=view;name=RNN%A4%C8SOAP4R%A4%C7%CD%B7%A4%DC%A4%A6
+# を参照してください。
+
+
+require 'soap/wsdlDriver'
+wsdl = 'http://cvs.sourceforge.jp/cgi-bin/viewcvs.cgi/*checkout*/rnn/rnn/app/rnn-hash.wsdl'
+rnn = SOAP::WSDLDriverFactory.new(wsdl).createDriver
+rnn.generateEncodeType = true
+#rnn.setWireDumpDev(STDERR)
+
+test_article_id = 1
+POST_COMMENT_DIRECT = 0
+
+
+# 日付で新しい順にソートして，pos番目からn個のニュースを取り出します
+pos = 0
+n = 5
+topicid = nil
+puts rnn.list(pos, n, topicid)
+
+# IDが id の記事を取得します
+rnn.article(test_article_id).each do |k, v|
+  puts "#{k}: #{v}"
+end
+
+# IDが id の記事についてのすべてのコメントを取得します
+puts rnn.comments(test_article_id)
+
+# 最近 days 日間の記事を取得します
+days = 1
+topic = nil
+rnn.recent_articles(days, topic).each do |article|
+  article.each do |k, v|
+    puts "#{k}: #{v}"
+  end
+end
+
+# 最近 days 日間のコメントを取得します
+days = 1
+rnn.recent_comments(days).each do |comment|
+  comment.each do |k, v|
+    puts "#{k}: #{v}"
+  end
+end
+
+# トピックの分類一覧を取得します
+rnn.topics.each do |topic|
+  topic.each do |k, v|
+    puts "#{k}: #{v}"
+  end
+end
---
+++ sample/RNN/post.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby -Ke
+
+
+# RNNのSOAPサービスを利用するサンプルです。テスト記事に対するコメントを
+# 投稿します。実行する前に、
+# http://cvs.sourceforge.jp/cgi-bin/viewcvs.cgi/*checkout*/rnn/rnn/doc/articles/xmlrpc.txt
+# および
+# http://rwiki.jin.gr.jp/cgi-bin/rw-cgi.rb?cmd=view;name=RNN%A4%C8SOAP4R%A4%C7%CD%B7%A4%DC%A4%A6
+# を参照してください。
+
+
+require 'soap/wsdlDriver'
+wsdl = 'http://cvs.sourceforge.jp/cgi-bin/viewcvs.cgi/*checkout*/rnn/rnn/app/rnn-hash.wsdl'
+rnn = SOAP::WSDLDriverFactory.new(wsdl).createDriver
+rnn.generateEncodeType = true
+#rnn.setWireDumpDev(STDERR)
+
+
+test_article_id = 1
+POST_COMMENT_DIRECT = 0
+
+subject = "SOAP4Rによるテスト by 名無しさん"
+text =<<__EOS__
+テスト投稿です。
+__EOS__
+
+p rnn.post_comment(test_article_id, POST_COMMENT_DIRECT, subject, text)
