4b456696e1ea1cde1c293c6001533b4e5803915a
nahi <nahi@ruby-lang.org>
2003-01-17 16:52:42 +0000

    Omit log.

--- sample/GoogleSearch/wsdlDriver.rb
+++ sample/GoogleSearch/wsdlDriver.rb
@@ -5,8 +5,8 @@ searchWord = ARGV.shift
 # You must get key from http://www.google.com/apis/ to use Google Web APIs.
 key = File.open(File.expand_path("~/.google_key")).read.chomp
 
-#GOOGLE_WSDL = 'http://api.google.com/GoogleSearch.wsdl'
-GOOGLE_WSDL = 'GoogleSearch.wsdl'
+GOOGLE_WSDL = 'http://api.google.com/GoogleSearch.wsdl'
+#GOOGLE_WSDL = 'GoogleSearch.wsdl'
 
 def html2rd(str)
   str.gsub(%r(<b>(.*?)</b>), '((*\\1*))').strip
@@ -14,7 +14,7 @@ end
 
 
 google = SOAP::WSDLDriverFactory.new(GOOGLE_WSDL).createDriver
-google.setWireDumpDev(STDERR)
+#google.setWireDumpDev(STDERR)
 result = google.doGoogleSearch( key, searchWord, 0, 10, false, "", false, "",
   'utf-8', 'utf-8' )
 result.resultElements.each do |ele|
