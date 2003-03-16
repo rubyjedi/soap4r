937fae169b711054336f502c5df612c26cc85cf4
Hiroshi Nakamura <nakahiro@gmail.com>
2003-03-16 11:13:02 +0000

    Move to rpc/* or mapping/*.  Add constant definitions for backward compatibility.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@786 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- lib/soap/server.rb
+++ lib/soap/server.rb
@@ -1,6 +1,6 @@
 =begin
 SOAP4R - Server implementation
-Copyright (c) 2001 NAKAMURA, Hiroshi
+Copyright (c) 2003 NAKAMURA, Hiroshi
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -17,85 +17,9 @@ Ave, Cambridge, MA 02139, USA.
 =end
 
 
-require 'soap/rpcRouter'
-require 'devel/logger'
+require 'soap/rpc/server'
 
 
 module SOAP
-
-
-###
-# SYNOPSIS
-#   Server.new( appName, namespace )
-#
-# DESCRIPTION
-#   To be written...
-#
-class Server < Devel::Application
-  include SOAP
-
-  def initialize( appName, namespace = nil )
-    super( appName )
-    @namespace = namespace
-    @router = RPCRouter.new( appName )
-    methodDef
-  end
- 
-  def mappingRegistry
-    @router.mappingRegistry
-  end
-
-  def mappingRegistry=( value )
-    @router.mappingRegistry = value
-  end
-
-  def addServant( obj, namespace = @namespace )
-   ( obj.methods - Kernel.instance_methods ).each do | methodName |
-      method = obj.method( methodName )
-      paramDef = RPCUtils::SOAPMethod.createParamDef(
-	( 1..method.arity.abs ).collect { |i| "p#{ i }" } )
-      @router.addMethod( namespace, obj, methodName, paramDef )
-    end
-  end
-
-protected
-  
-  def methodDef
-    # Override this method in derived class to call 'addMethod*' to add methods.
-  end
-
-  def addMethod( receiver, methodName, *paramArg )
-    addMethodWithNSAs( @namespace, receiver, methodName, methodName, *paramArg )
-  end
-
-  def addMethodAs( receiver, methodName, methodNameAs, *paramArg )
-    addMethodWithNSAs( @namespace, receiver, methodName, methodNameAs,
-      *paramArg )
-  end
-
-  def addMethodWithNS( namespace, receiver, methodName, *paramArg )
-    addMethodWithNSAs( namespace, receiver, methodName, methodName, *paramArg )
-  end
-
-  def addMethodWithNSAs( namespace, receiver, methodName, methodNameAs,
-      *paramArg )
-    paramDef = if paramArg.size == 1 and paramArg[ 0 ].is_a?( Array )
-        paramArg[ 0 ]
-      else
-        RPCUtils::SOAPMethod.createParamDef( paramArg )
-      end
-    @router.addMethodAs( namespace, receiver, methodName, methodNameAs,
-      paramDef )
-  end
-
-  def route( requestString, charset )
-    @router.route( requestString, charset )
-  end
-
-  def createFaultResponseString( e )
-    @router.createFaultResponseString( e )
-  end
-end
-
-
+  Server = RPC::Server
 end
