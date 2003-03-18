4f301227ceb1c7d4337519fb2d251c833fea73ef
nahi <nahi@ruby-lang.org>
2003-03-18 15:12:15 +0000

    Change implementation to use soap/rpc/standaloneServer.rb.   appName is ignored.

--- lib/soap/standaloneServer.rb
+++ lib/soap/standaloneServer.rb
@@ -16,9 +16,7 @@ this program; if not, write to the Free Software Foundation, Inc., 675 Mass
 Ave, Cambridge, MA 02139, USA.
 =end
 
-require 'soap/server'
-require 'soap/streamHandler'
-require "soap/httpserver"
+require 'soap/rpc/standaloneServer'
 
 
 module SOAP
@@ -26,92 +24,14 @@ module SOAP
 
 ###
 # SYNOPSIS
-#   StandaloneServer.new( appName, namespace, listening_i/f, listening_port )
+#   StandaloneServer.new(appName, namespace, listening_i/f, listening_port)
 #
 # DESCRIPTION
-#   To be written...
+#   appName is ignored.
 #
-class StandaloneServer < Server
-  include SOAP
-
-  class SAError < Error; end
-  
-  ALLOWED_LENGTH = 1024 * 1024
-    
-  def initialize( appName, namespace, host = "127.0.0.1", port = 8080 )
-    super( appName, namespace )
-    @host, @port = host, port
-
-    handler = self.method( :request_handler ).to_proc
-    @server = ::HttpServer.new(handler, @port, @host)
-  end
-  
-protected
-  
-  def methodDef
-    # Override this method in derived class to call 'addMethod' to add methods.
-  end
-
-private
-
-  def request_handler(request, response)
-    log( SEV_INFO ) { "Received a request." }
-    
-    if request.method != 'POST'
-      raise SAError.new( "Method '#{ request.method }' not allowed." )
-    end
-    
-    length = request.content_length || 0
-    if length > ALLOWED_LENGTH
-      raise SAError.new( "Content-length too long." )
-    end
-
-    log( SEV_INFO ) { "Request: method: #{ request.method }, size: #{ length }" }
-
-    contentType = request.header['Content-Type']
-    requestCharset = StreamHandler.parseMediaType( contentType )
-    requestString = request.data.read( length )        
-    log( SEV_DEBUG ) { "XML Request: #{requestString}" }
-
-    responseString, isFault = route( requestString, requestCharset )
-    log( SEV_DEBUG ) { "XML Response: #{responseString}" }
-    
-    unless isFault
-      response.status = 200
-    else
-      response.status = 500
-    end
-    response.header['Cache-Control'] = 'private'  
-    response.header['Content-Type'] = StreamHandler.createMediaType(
-      requestCharset )
-    response.header['Content-Length'] = responseString.length
-    response.body = responseString
-
-  rescue Exception
-    responseString  = createFaultResponseString( $! )
-    response.body   = responseString
-    response.status = 500
-    response.header['Content-Type'] = StreamHandler.createMediaType(
-      requestCharset )
-
-  end
-  
-  def run
-    class <<@log
-      def puts( msg )
-	add( SEV_INFO, msg, "SOAP::StandaloneServer" )
-      end
-      def flush; end
-    end
-    @server.stdlog = @log
-
-    begin
-      trap( 'INT' ) { @server.shutdown; exit }
-      trap( 'HUP' ) { @server.shutdown; exit }
-    rescue ArgumentError
-      # mswin32 ruby cannot handle SIGHUP because of Windows' restriction.
-    end
-    @server.start.join
+class StandaloneServer < RPC::StandaloneServer
+  def initialize(appName, namespace, host = "127.0.0.1", port = 8080)
+    super(namespace, host, port)
   end
 end
 
