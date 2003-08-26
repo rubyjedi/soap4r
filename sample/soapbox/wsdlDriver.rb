8e5c63597ad88e3304d28051ef4666ae26188650
nahi <nahi@ruby-lang.org>
2003-08-26 14:51:50 +0000

    WSDL file was changed!

--- sample/soapbox/wsdlDriver.rb
+++ sample/soapbox/wsdlDriver.rb
@@ -1,4 +1,12 @@
 require 'soap/wsdlDriver'
+include SOAP
 
 wsdl = ARGV.shift || 'SoapBoxWebService.wsdl'
 driver = SOAP::WSDLDriverFactory.new(wsdl).create_driver
+driver.wiredump_dev = STDOUT
+
+userinfo = SOAPElement.new(XSD::QName.new('http://www.winfessor.com/SoapBoxWebService/SoapBoxWebService', 'RegisterUser'))
+userinfo.add(SOAPElement.new('username', 'NaHi'))
+userinfo.add(SOAPElement.new('passowrd', 'passwd'))
+userinfo.add(SOAPElement.new('hostname', 'www.example.com'))
+driver.RegisterUser(nil, userinfo)
