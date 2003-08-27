ffa4fc15f74085be5bca5ad0f12da3d3efef4b3d
Hiroshi Nakamura <nakahiro@gmail.com>
2003-08-27 15:51:02 +0000

    Still does not work.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@888 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- sample/soapbox/wsdlDriver.rb
+++ sample/soapbox/wsdlDriver.rb
@@ -5,8 +5,11 @@ wsdl = ARGV.shift || 'SoapBoxWebService.wsdl'
 driver = SOAP::WSDLDriverFactory.new(wsdl).create_driver
 driver.wiredump_dev = STDOUT
 
-userinfo = SOAPElement.new(XSD::QName.new('http://www.winfessor.com/SoapBoxWebService/SoapBoxWebService', 'RegisterUser'))
-userinfo.add(SOAPElement.new('username', 'NaHi'))
-userinfo.add(SOAPElement.new('passowrd', 'passwd'))
-userinfo.add(SOAPElement.new('hostname', 'www.example.com'))
+#userinfo = SOAPElement.new(XSD::QName.new('http://www.winfessor.com/SoapBoxWebService/SoapBoxWebService', 'RegisterUser'))
+#userinfo.add(SOAPElement.new('username', 'NaHi'))
+#userinfo.add(SOAPElement.new('passowrd', 'passwd'))
+#userinfo.add(SOAPElement.new('hostname', 'www.example.com'))
+
+s = Struct.new(:username, :password, :hostname)
+userinfo = s.new('NaHi', 'nahi', 'jabber.example.com')
 driver.RegisterUser(nil, userinfo)
