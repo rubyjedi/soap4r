0fd747e473a65a90e67ca9d16b16cbae86d94794
nahi <nahi@ruby-lang.org>
2004-01-15 15:09:56 +0000

    * header handler implementation

---
+++ lib/soap/header/handlerset.rb
@@ -0,0 +1,49 @@
+# SOAP4R - SOAP Header handler set
+# Copyright (C) 2003, 2004  NAKAMURA, Hiroshi <nahi@ruby-lang.org>.
+
+# This program is copyrighted free software by NAKAMURA, Hiroshi.  You can
+# redistribute it and/or modify it under the same terms of Ruby's license;
+# either the dual license version in 2003, or any later version.
+
+
+require 'xsd/namedelements'
+require 'soap/header/item'
+
+
+module SOAP
+module Header
+
+
+class HandlerSet
+  def initialize
+    @store = XSD::NamedElements.new
+  end
+
+  def add(handler)
+    @store << handler
+  end
+  alias << add
+
+  def delete(handler)
+    @store.delete(handler)
+  end
+
+  # returns: Array of SOAPHeaderItem
+  def on_outbound
+    @store.collect { |handler|
+      handler.on_outbound_headeritem
+    }.compact
+  end
+
+  # headers: SOAPHeaderItem enumerable object
+  def on_inbound(headers)
+    @store.each do |handler|
+      name, item = headers.find { |header| handler.name == header.elename }
+      handler.on_inbound_headeritem(item)
+    end
+  end
+end
+
+
+end
+end
