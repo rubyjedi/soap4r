8f900c188c3dba6cd0729b12a35235408e29e77c
nahi <nahi@ruby-lang.org>
2004-05-16 10:27:22 +0000

    * revert the previous signature change.  preparing for simpleType class   generation support.

--- lib/wsdl/soap/classDefCreator.rb
+++ lib/wsdl/soap/classDefCreator.rb
@@ -18,9 +18,9 @@ module SOAP
 class ClassDefCreator
   include ClassDefCreatorSupport
 
-  def initialize(complextypes, definitions = nil)
-    @complextypes = complextypes
-    @faulttypes = definitions ? collect_faulttype(definitions) : []
+  def initialize(definitions)
+    @complextypes = definitions.collect_complextypes
+    @faulttypes = definitions.collect_faulttypes
   end
 
   def dump(class_name = nil)
@@ -48,7 +48,8 @@ private
   def dump_classdef(qname)
     complextype = @complextypes[qname]
     if @faulttypes.index(qname)
-      c = XSD::CodeGen::ClassDef.new(create_class_name(qname), ::StandardError)
+      c = XSD::CodeGen::ClassDef.new(create_class_name(qname),
+        "::StandardError")
     else
       c = XSD::CodeGen::ClassDef.new(create_class_name(qname))
     end
@@ -75,34 +76,6 @@ private
     c.def_classvar("schema_type", qname.name.dump)
     c.def_classvar("schema_ns", qname.namespace.dump)
     c.dump
-  end
-
-  def collect_faulttype(definitions)
-    result = []
-    collect_fault_messages(definitions).each do |message|
-      parts = definitions.message(message).parts
-      if parts.size != 1
-	raise RuntimeError.new("Expects fault message to have 1 part.")
-      end
-      if result.index(parts[0].type).nil?
-	result << parts[0].type
-      end
-    end
-    result
-  end
-
-  def collect_fault_messages(definitions)
-    result = []
-    definitions.porttypes.each do |porttype|
-      porttype.operations.each do |operation|
-	operation.fault.each do |fault|
-	  if result.index(fault.message).nil?
-	    result << fault.message
-	  end
-	end
-      end
-    end
-    result
   end
 end
 
