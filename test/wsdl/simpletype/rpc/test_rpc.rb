3a5efb9322f140fc52b0b4cde9767ecf76a80472
Hiroshi Nakamura <nakahiro@gmail.com>
2004-05-16 15:15:01 +0000

    * add --classdef test
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@1244 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- test/wsdl/simpletype/rpc/test_rpc.rb
+++ test/wsdl/simpletype/rpc/test_rpc.rb
@@ -11,6 +11,7 @@ class TestRPC < Test::Unit::TestCase
 
   def test_rpc
     system("cd #{DIR} && ruby #{pathname("../../../../bin/wsdl2ruby.rb")} --classdef --wsdl #{pathname("rpc.wsdl")} --type client --type server --force --quiet")
+    compare("expectedEchoVersion.rb", "echo_version.rb")
     compare("expectedDriver.rb", "echo_versionDriver.rb")
     compare("expectedService.rb", "echo_version_service.rb")
 
