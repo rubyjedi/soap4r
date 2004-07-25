af2eb6b22b360dc7c7e60866a337560408a718c4
nahi <nahi@ruby-lang.org>
2004-07-25 13:49:01 +0000

    * refer constant explicit.

--- sample/soap/netDicV06/netDicV06.rb
+++ sample/soap/netDicV06/netDicV06.rb
@@ -10,7 +10,7 @@ wiredump_dev = STDERR       # STDERR
 
 dic = SOAP::RPC::Driver.new(server, INetDicV06::InterfaceNS)
 dic.wiredump_dev = wiredump_dev
-dic.default_encodingstyle = SOAP::EncodingStyleHandlerASPDotNet::Namespace
+dic.default_encodingstyle = ::SOAP::EncodingStyle::ASPDotNetHandler::Namespace
 INetDicV06::add_method(dic)
 
 p dic.GetDicList()
