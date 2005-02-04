acee130177315a2298ef2334aaf87373a1f40443
Hiroshi Nakamura <nakahiro@gmail.com>
2005-02-04 01:58:23 +0000

    move instance_variable_{get|set} definition.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@1420 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- lib/soap/soap.rb
+++ lib/soap/soap.rb
@@ -101,6 +101,20 @@ class FaultError < Error
   end
 end
 
+
+unless Object.respond_to?(:instance_variable_get)
+  class Object
+    def instance_variable_get(ivarname)
+      instance_eval(ivarname)
+    end
+
+    def instance_variable_set(ivarname, value)
+      instance_eval("#{ivarname} = value")
+    end
+  end
+end
+
+
 module Env
   def self.getenv(name)
     ENV[name.downcase] || ENV[name.upcase]
