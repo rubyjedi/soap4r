e53bda96a030d236c23bd33dd6d6bc9a29a4c993
nahi <nahi@ruby-lang.org>
2005-02-22 00:51:36 +0000

    add a custom Array factory sample.

--- sample/soap/customfactory.rb
+++ sample/soap/customfactory.rb
@@ -50,10 +50,49 @@ class DummyStructFactory
   end
 end
 
+class ExplicitTypedArrayFactory < SOAP::Mapping::Factory
+  def obj2soap(soap_class, obj, info, map)
+    unless obj.respond_to?(:arytype)
+      return nil
+    end
+    soap_obj = soap_class.new(ValueArrayName, 1, obj.arytype)
+    mark_marshalled_obj(obj, soap_obj)
+    obj.each do |item|
+      child = Mapping._obj2soap(item, map)
+      soap_obj.add(child)
+    end
+    soap_obj
+  end
+
+  # let default TypedArrayFactory handle that.
+  def soap2obj(obj_class, node, info, map)
+    return false
+  end
+end
+
 map = Mapping::Registry.new
 map.set(DummyStruct, SOAPStruct, DummyStructFactory.new)
+map.set(Array, SOAPArray, ExplicitTypedArrayFactory.new)
+
+puts
+puts "== DummyStruct =="
+puts
 
 obj = DummyStruct.new('family' => 'Na', 'given' => 'Hi')
 puts marshalledstring = SOAPMarshal.marshal(obj, map)
+p SOAPMarshal.unmarshal(marshalledstring, map)
 
+puts
+puts "== Custom array =="
+puts
+
+obj1 = [1, 2, 3]
+obj2 = [4, 5, 6]
+def obj1.arytype
+  XSD::XSDInt::Type
+end
+
+puts marshalledstring = SOAPMarshal.marshal(obj1, map)
+p SOAPMarshal.unmarshal(marshalledstring, map)
+puts marshalledstring = SOAPMarshal.marshal(obj2, map)
 p SOAPMarshal.unmarshal(marshalledstring, map)
