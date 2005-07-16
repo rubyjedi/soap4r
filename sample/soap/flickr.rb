eafef4b2245cee82fca35f1ca8cb9ec9daaa74e5
Hiroshi Nakamura <nakahiro@gmail.com>
2005-07-16 14:24:04 +0000

    add a sample client of Flickr SOAP interface: http://www.flickr.com/services/api/request.soap.html closes #122.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@1577 23c4dcf6-64ea-0310-84d7-d424c7495ec0

---
+++ sample/soap/flickr.rb
@@ -0,0 +1,25 @@
+require 'soap/rpc/driver'
+
+api_key = ARGV.shift or raise
+
+flickr = SOAP::RPC::Driver.new('http://www.flickr.com/services/soap/')
+
+flickr.add_document_method('request', nil,
+  XSD::QName.new('urn:flickr', 'FlickrRequest'),
+  XSD::QName.new('urn:flickr', 'FlickrResponse'))
+
+soap12namespace = 'http://www.w3.org/2003/05/soap-envelope'
+flickr.options['soap.envelope.requestnamespace'] = soap12namespace
+flickr.options['soap.envelope.responsenamespace'] = soap12namespace
+
+response = flickr.request(
+  :api_key => api_key,
+  :method => 'flickr.test.echo',
+  :name => 'hello world')
+
+responsexml = "<dummy>#{response}</dummy>"
+
+require 'xsd/mapping'
+obj = XSD::Mapping.xml2obj(responsexml)
+p obj.method
+p obj.name
