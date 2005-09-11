f655ca10301a5eeda805953aba289ac39b9b3224
Hiroshi Nakamura <nakahiro@gmail.com>
2005-09-11 11:59:35 +0000

    sample updated.  Thanks to Juha.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@1611 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- sample/wsdl/documentliteral/client.rb
+++ sample/wsdl/documentliteral/client.rb
@@ -3,8 +3,9 @@ require 'defaultDriver.rb'
 
 endpoint_url = ARGV.shift
 obj = EchoPortType.new(endpoint_url)
+obj.wiredump_dev = STDOUT if $DEBUG
 
 request = EchoRequest.new
-request.attr_sampleAttr = 5
+request.xmlattr_sampleAttr = 5
 request.sampleElement = 3.14
 p obj.echo(request)
--- sample/wsdl/documentliteral/servant.rb
+++ sample/wsdl/documentliteral/servant.rb
@@ -3,7 +3,7 @@ require 'default.rb'
 class EchoPortType
   def echo(parameters)
     response = EchoResponse.new
-    response.sampleMultiValue << parameters.attr_sampleAttr
+    response.sampleMultiValue << parameters.xmlattr_sampleAttr
     response.sampleMultiValue << parameters.sampleElement
     response
   end
