49aa07e7690a85a842c59c63a9b76b8f245d46b3
nahi <nahi@ruby-lang.org>
2005-09-13 14:26:57 +0000

    test for qualified and unqualified

--- test/wsdl/qualified/test_unqualified.rb
+++ test/wsdl/qualified/test_unqualified.rb
@@ -8,21 +8,21 @@ if defined?(HTTPAccess2)
 module WSDL
 
 
-class TestQualified < Test::Unit::TestCase
+class TestUnqualified < Test::Unit::TestCase
   class Server < ::SOAP::RPC::StandaloneServer
-    Namespace = 'http://www50.brinkster.com/vbfacileinpt/np'
+    Namespace = 'urn:lp'
 
     def on_init
       add_document_method(
         self,
-        Namespace + '/GetPrimeNumbers',
-        'GetPrimeNumbers',
-        XSD::QName.new(Namespace, 'GetPrimeNumbers'),
-        XSD::QName.new(Namespace, 'GetPrimeNumbersResponse')
+        Namespace + ':login',
+        'login',
+        XSD::QName.new(Namespace, 'login'),
+        XSD::QName.new(Namespace, 'loginResponse')
       )
     end
   
-    def GetPrimeNumbers(arg)
+    def login(arg)
       nil
     end
   end
@@ -38,10 +38,8 @@ class TestQualified < Test::Unit::TestCase
 
   def teardown
     teardown_server
-    unless $DEBUG
-      File.unlink(pathname('default.rb'))
-      File.unlink(pathname('defaultDriver.rb'))
-    end
+    File.unlink(pathname('lp.rb'))
+    File.unlink(pathname('lpDriver.rb'))
     @client.reset_stream if @client
   end
 
@@ -56,14 +54,14 @@ class TestQualified < Test::Unit::TestCase
     begin
       Dir.chdir(DIR)
       gen = WSDL::SOAP::WSDL2Ruby.new
-      gen.location = pathname("np.wsdl")
+      gen.location = pathname("lp.wsdl")
       gen.basedir = DIR
       gen.logger.level = Logger::FATAL
       gen.opt['classdef'] = nil
       gen.opt['driver'] = nil
       gen.opt['force'] = true
       gen.run
-      require pathname('default.rb')
+      require pathname('lp')
     ensure
       Dir.chdir(backupdir)
     end
@@ -87,32 +85,22 @@ class TestQualified < Test::Unit::TestCase
     File.join(DIR, filename)
   end
 
-  LOGIN_REQUEST_QUALIFIED_NS =
-%q[<?xml version="1.0" encoding="utf-8" ?>
-<env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:env="http://schemas.xmlsoap.org/soap/envelope/"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <env:Body>
-    <n1:GetPrimeNumbers xmlns:n1="http://www50.brinkster.com/vbfacileinpt/np">
-      <n1:Max>10</n1:Max>
-    </n1:GetPrimeNumbers>
-  </env:Body>
-</env:Envelope>]
-
   LOGIN_REQUEST_QUALIFIED =
 %q[<?xml version="1.0" encoding="utf-8" ?>
 <env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema"
     xmlns:env="http://schemas.xmlsoap.org/soap/envelope/"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <env:Body>
-    <GetPrimeNumbers xmlns="http://www50.brinkster.com/vbfacileinpt/np">
-      <Max>10</Max>
-    </GetPrimeNumbers>
+    <n1:login xmlns:n1="urn:lp">
+      <username>NaHi</username>
+      <password>passwd</password>
+      <timezone>JST</timezone>
+    </n1:login>
   </env:Body>
 </env:Envelope>]
 
   def test_wsdl
-    wsdl = File.join(DIR, 'np.wsdl')
+    wsdl = File.join(DIR, 'lp.wsdl')
     @client = nil
     backupdir = Dir.pwd
     begin
@@ -123,8 +111,9 @@ class TestQualified < Test::Unit::TestCase
     end
     @client.endpoint_url = "http://localhost:#{Port}/"
     @client.wiredump_dev = str = ''
-    @client.GetPrimeNumbers(:Max => 10)
-    assert_equal(LOGIN_REQUEST_QUALIFIED_NS, parse_requestxml(str))
+    @client.login(:timezone => 'JST', :password => 'passwd',
+      :username => 'NaHi')
+    assert_equal(LOGIN_REQUEST_QUALIFIED, parse_requestxml(str))
   end
 
   include ::SOAP
@@ -132,14 +121,14 @@ class TestQualified < Test::Unit::TestCase
     backupdir = Dir.pwd
     begin
       Dir.chdir(DIR)
-      require pathname('defaultDriver')
+      require pathname('lpDriver')
     ensure
       Dir.chdir(backupdir)
     end
-    @client = PnumSoap.new("http://localhost:#{Port}/")
+    @client = Lp_porttype.new("http://localhost:#{Port}/")
 
     @client.wiredump_dev = str = ''
-    @client.getPrimeNumbers(GetPrimeNumbers.new(10))
+    @client.login(Login.new('NaHi', 'passwd', 'JST'))
     assert_equal(LOGIN_REQUEST_QUALIFIED, parse_requestxml(str))
   end
 
