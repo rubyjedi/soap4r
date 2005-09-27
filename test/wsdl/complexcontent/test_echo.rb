7807561e90efd178d1f5b9472a0b3b907f27114e
nahi <nahi@ruby-lang.org>
2005-09-27 14:31:08 +0000

    add a test of complexContent + elementFormDefault.  closes #151.

---
+++ test/wsdl/complexcontent/test_echo.rb
@@ -0,0 +1,90 @@
+require 'test/unit'
+require 'wsdl/parser'
+require 'wsdl/soap/wsdl2ruby'
+require 'soap/rpc/standaloneServer'
+require 'soap/wsdlDriver'
+
+
+module WSDL; module ComplexContent
+
+
+class TestEcho < Test::Unit::TestCase
+  class Server < ::SOAP::RPC::StandaloneServer
+    Namespace = 'urn:complexContent'
+
+    def on_init
+      add_document_method(
+        self,
+        nil,
+        'echo',
+        XSD::QName.new(Namespace, 'echo'),
+        XSD::QName.new(Namespace, 'echo')
+      )
+    end
+  
+    def echo(arg)
+      arg
+    end
+  end
+
+  DIR = File.dirname(File.expand_path(__FILE__))
+  Port = 17171
+
+  def setup
+    setup_server
+    setup_classdef
+    @client = nil
+  end
+
+  def teardown
+    teardown_server
+    File.unlink(pathname('complexContent.rb')) unless $DEBUG
+    @client.reset_stream if @client
+  end
+
+  def setup_server
+    @server = Server.new('Test', Server::Namespace, '0.0.0.0', Port)
+    @server.level = Logger::Severity::ERROR
+    @server_thread = start_server_thread(@server)
+  end
+
+  def setup_classdef
+    gen = WSDL::SOAP::WSDL2Ruby.new
+    gen.location = pathname("complexContent.wsdl")
+    gen.basedir = DIR
+    gen.logger.level = Logger::FATAL
+    gen.opt['classdef'] = nil
+    gen.opt['force'] = true
+    gen.run
+    require pathname('complexContent')
+  end
+
+  def teardown_server
+    @server.shutdown
+    @server_thread.kill
+    @server_thread.join
+  end
+
+  def start_server_thread(server)
+    t = Thread.new {
+      Thread.current.abort_on_exception = true
+      server.start
+    }
+    t
+  end
+
+  def pathname(filename)
+    File.join(DIR, filename)
+  end
+
+  def test_wsdl
+    wsdl = File.join(DIR, 'complexContent.wsdl')
+    @client = ::SOAP::WSDLDriverFactory.new(wsdl).create_rpc_driver
+    @client.endpoint_url = "http://localhost:#{Port}/"
+    @client.wiredump_dev = STDOUT if $DEBUG
+    assert_instance_of(Echo, @client.echo(Echo.new(Derived.new)))
+  end
+end
+
+
+end; end
