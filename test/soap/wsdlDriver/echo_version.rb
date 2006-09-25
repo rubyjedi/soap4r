423e2fae19a583647f07d29541bde767501baa2f
Hiroshi Nakamura <nakahiro@gmail.com>
2006-09-25 06:43:32 +0000

    updated classdef file for a test.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@1728 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- test/soap/wsdlDriver/echo_version.rb
+++ test/soap/wsdlDriver/echo_version.rb
@@ -1,20 +1,29 @@
-# urn:example.com:simpletype-rpc-type
+require 'xsd/qname'
+
+# {urn:example.com:simpletype-rpc-type}version_struct
 class Version_struct
   @@schema_type = "version_struct"
   @@schema_ns = "urn:example.com:simpletype-rpc-type"
+  @@schema_element = [
+    ["myversion", ["SOAP::SOAPString", XSD::QName.new(nil, "myversion")]],
+    ["msg", ["SOAP::SOAPString", XSD::QName.new(nil, "msg")]]
+  ]
 
-  attr_accessor :version
+  attr_accessor :myversion
   attr_accessor :msg
 
-  def initialize(version = nil, msg = nil)
-    @version = version
+  def initialize(myversion = nil, msg = nil)
+    @myversion = myversion
     @msg = msg
   end
 end
 
-# urn:example.com:simpletype-rpc-type
-module Versions
-  C_16 = "1.6"
-  C_18 = "1.8"
-  C_19 = "1.9"
+# {urn:example.com:simpletype-rpc-type}myversions
+class Myversions < ::String
+  @@schema_type = "myversions"
+  @@schema_ns = "urn:example.com:simpletype-rpc-type"
+
+  C_16 = Myversions.new("1.6")
+  C_18 = Myversions.new("1.8")
+  C_19 = Myversions.new("1.9")
 end
