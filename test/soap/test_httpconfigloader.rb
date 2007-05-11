7add21edc36445b7ba0b000ad5c0db2041d2d161
nahi <nahi@ruby-lang.org>
2007-05-11 05:53:28 +0000

    * allow to set basic_auth definition in soap/property file.  closes #198.  * do not do pretty-printing when a SOAPElement does not have any elements.  closes #336.

--- test/soap/test_httpconfigloader.rb
+++ test/soap/test_httpconfigloader.rb
@@ -18,15 +18,32 @@ class TestHTTPConfigLoader < Test::Unit::TestCase
     testpropertyname = File.join(DIR, 'soapclient.properties')
     File.open(testpropertyname, "w") do |f|
       f <<<<__EOP__
+protocol.http.proxy = http://myproxy:8080
 protocol.http.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_PEER
 # depth: 1 causes an error (intentional)
 protocol.http.ssl_config.verify_depth = 1
 protocol.http.ssl_config.ciphers = ALL
+protocol.http.basic_auth.1.url = http://www.example.com/foo1/
+protocol.http.basic_auth.1.userid = user1
+protocol.http.basic_auth.1.password = password1
+protocol.http.basic_auth.2.url = http://www.example.com/foo2/
+protocol.http.basic_auth.2.userid = user2
+protocol.http.basic_auth.2.password = password2
 __EOP__
     end
     begin
       @client.loadproperty(testpropertyname)
       assert_equal('ALL', @client.options['protocol.http.ssl_config.ciphers'])
+      @client.options['protocol.http.basic_auth'] <<
+        ['http://www.example.com/foo3/', 'user3', 'password3']
+      h = @client.streamhandler.client
+      basic_auth = h.instance_eval { @basic_auth }
+      cred1 = ["user1:password1"].pack('m').tr("\n", '')
+      cred2 = ["user2:password2"].pack('m').tr("\n", '')
+      cred3 = ["user3:password3"].pack('m').tr("\n", '')
+      assert_equal(cred1, basic_auth.get(URI.parse("http://www.example.com/foo1/baz")))
+      assert_equal(cred2, basic_auth.get(URI.parse("http://www.example.com/foo2/")))
+      assert_equal(cred3, basic_auth.get(URI.parse("http://www.example.com/foo3/baz/qux")))
     ensure
       File.unlink(testpropertyname)
     end
