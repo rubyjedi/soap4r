c3f8a5400b5e60371d7748c39bdb18c747cf4967
Hiroshi Nakamura <nakahiro@gmail.com>
2007-08-23 11:52:08 +0000

    * allow <any> appear twice or more (with the current implementation, soap4r ignores <any> element definition such as namespace and processContent.  just allow any element appear.)  closes #402.
    git-svn-id: http://dev.ctor.org/svn/soap4r/trunk@1918 23c4dcf6-64ea-0310-84d7-d424c7495ec0

--- lib/wsdl/xmlSchema/choice.rb
+++ lib/wsdl/xmlSchema/choice.rb
@@ -44,7 +44,6 @@ class Choice < Content
       @elements << o
       o
     when AnyName
-      raise ElementConstraintError.new("duplicated element: any") if @any
       @any = Any.new
       @elements << @any
       @any
