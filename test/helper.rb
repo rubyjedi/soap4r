9929deb5218cf882345debe40aeabf32912e6edf
Laurence A. Lee <rubyjedi@gmail.com>
2010-06-14 11:41:33 -1000

    Add support for toggling Wiredumps in Rake Tests via ENV['WIREDUMPS']

--- test/helper.rb
+++ test/helper.rb
@@ -3,3 +3,4 @@ require 'rubygems'
 require 'test/unit/xml' ## RubyJedi
 
 ENV['DEBUG_SOAP4R'] = 'true' ## Needed to force wsdl2ruby.rb and xsd2ruby.rb to use DEVELOPMENT soap4r libs instead of installed soap4r libs
+$DEBUG = !!ENV['WIREDUMPS']
