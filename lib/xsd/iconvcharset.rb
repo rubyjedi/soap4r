cef0da77aba3ac25426d6cac208669869662a393
Laurence Lee <rubyjedi@gmail.com>
2015-06-09 22:33:51 -1000

    Tweak for Ruby 1.9 and above (iconv deprecated, use String#encode instead)

--- lib/xsd/iconvcharset.rb
+++ lib/xsd/iconvcharset.rb
@@ -6,15 +6,14 @@
 # redistribute it and/or modify it under the same terms of Ruby's license;
 # either the dual license version in 2003, or any later version.
 
-
-require 'iconv'
-
+require 'iconv' unless ''.respond_to? :encode
 
 module XSD
 
 
 class IconvCharset
   def self.safe_iconv(to, from, str)
+    return str.encode(to, from, :invalid => :replace, :undef => :replace, :replace => '?') if ''.respond_to? :encode
     iconv = Iconv.new(to, from)
     out = ""
     begin
