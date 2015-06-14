707d1b2e235761156ebdbf51e3a02d4b0a27f15d
Laurence Lee <rubyjedi@gmail.com>
2015-06-14 12:42:50 -1000

    Refine the XML Parser loading. SOAP4R_PARSERS environment variable introduced to allow a preferred XML Parser (ideal case), or preferred XML Parser load-order, in case multiple XML parsers are available.

--- lib/xsd/xmlparser.rb
+++ lib/xsd/xmlparser.rb
@@ -6,60 +6,64 @@
 # redistribute it and/or modify it under the same terms of Ruby's license;
 # either the dual license version in 2003, or any later version.
 
-
 require 'xsd/xmlparser/parser'
 
+ module XSD
+  module XMLParser
 
-module XSD
-
-
-module XMLParser
-  def create_parser(host, opt)
-    XSD::XMLParser::Parser.create_parser(host, opt)
-  end
-  module_function :create_parser
-
-  # $1 is necessary.
-  NSParseRegexp = Regexp.new('^xmlns:?(.*)$', nil, 'n')
-
-  def filter_ns(ns, attrs)
-    ns_updated = false
-    if attrs.nil? or attrs.empty?
-      return [ns, attrs]
+    def create_parser(host, opt)
+      XSD::XMLParser::Parser.create_parser(host, opt)
     end
-    newattrs = {}
-    attrs.each do |key, value|
-      if NSParseRegexp =~ key
-        unless ns_updated
-          ns = ns.clone_ns
-          ns_updated = true
-        end
-	# tag == '' means 'default namespace'
-        # value == '' means 'no default namespace'
-	tag = $1 || ''
-	ns.assign(value, tag)
-      else
-	newattrs[key] = value
+    module_function :create_parser
+
+    # $1 is necessary.
+    NSParseRegexp = Regexp.new('^xmlns:?(.*)$', nil, 'n')
+
+    def filter_ns(ns, attrs)
+      ns_updated = false
+      if attrs.nil? or attrs.empty?
+        return [ns, attrs]
       end
+      newattrs = {}
+      attrs.each do |key, value|
+        if NSParseRegexp =~ key
+          unless ns_updated
+            ns = ns.clone_ns
+            ns_updated = true
+          end
+          # tag == '' means 'default namespace'
+          # value == '' means 'no default namespace'
+          tag = $1 || ''
+          ns.assign(value, tag)
+        else
+          newattrs[key] = value
+        end
+      end
+      return [ns, newattrs]
     end
-    return [ns, newattrs]
+    module_function :filter_ns
+
   end
-  module_function :filter_ns
 end
 
 
+# Provide a means to CHOOSE a preferred XML Parser and loading-order, if multiple parser gems are available.
+if ENV.has_key?('SOAP4R_PARSERS')
+  parser_list = ENV['SOAP4R_PARSERS'].to_s.split(',')
+else
+  parser_list = [
+    'oxparser',     ## Uses its own C-Ruby Extension
+    'nokogiri',     ## Depends on LibXML2, LibXSLT, and ZLib
+    'libxmlparser', ## Depends on LibXML2 ; Thread-Safe ; Somewhat Broken re: Namespaces
+    # ----- Pure-Ruby Parsers Below ---- #
+    'ogaparser',    ## Pure-Ruby
+    'rexmlparser',  ## Pure-Ruby, this is the reference implementation.
+  ]
 end
-
 
 # Try to load XML processor.
 loaded = false
-[
-  'nokogiri',
-#  'libxmlparser',
-#  'xmlparser',
-#  'xmlscanner',
-  'rexmlparser',
-].each do |name|
+parser_list.each do |name|
   begin
     require "xsd/xmlparser/#{name}"
     # XXX: for a workaround of rubygems' require inconsistency
