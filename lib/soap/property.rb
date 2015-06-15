909e4320c4ec8c4b1eb7c1e09d1bac3c9e43129b
Laurence Lee <rubyjedi@gmail.com>
2015-06-14 22:21:34 -1000

    Legacy Cleanup: Remove stuff specific to Ruby 1.6.

--- lib/soap/property.rb
+++ lib/soap/property.rb
@@ -324,17 +324,3 @@ end
 
 
 end
-
-
-# for ruby/1.6.
-#unless Enumerable.instance_methods.include?(:inject)
-#  module Enumerable
-#    def inject(init)
-#      result = init
-#      each do |item|
-#	result = yield(result, item)
-#      end
-#      result
-#    end
-#  end
-#end
