b42eeaee7066c0f14692e5b2088e14bb32c2d6f0
Vlad Bokov <razum2um@mail.ru>
2018-06-01 16:44:42 +0700

    Update codeclimate reporter -> use simplecov

--- test/helper.rb
+++ test/helper.rb
@@ -3,8 +3,8 @@ require 'test/unit'
 require 'test/unit/xml' ## RubyJedi
 
 if RUBY_VERSION.to_f >= 1.9
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
+  require 'simplecov'
+  SimpleCov.start
 end
 
 ENV['DEBUG_SOAP4R'] = 'true' ## Needed to force wsdl2ruby.rb and xsd2ruby.rb to use DEVELOPMENT soap4r libs instead of installed soap4r libs
